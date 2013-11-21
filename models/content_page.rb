224bc565a415a3895df423941a3fee6874ef50b5
Finn-Lennart Heemeyer <finn.heemeyer@ymail.com>
2013-11-21 20:19:25 +0000

    adds navigation and content page and model includer

--- models/content_page.rb
+++ models/content_page.rb
@@ -1,4 +1,4 @@
-class ContentPage
+class ContentPage < Page
   field :content
 
   belongs_to :parent, class_name: "NavigationPage", inverse_of: :children
---
+++ models/init.rb
@@ -0,0 +1,7 @@
+require_relative 'application'
+require_relative 'page'
+require_relative 'content_page'
+require_relative 'navigation_page'
+
+
+
